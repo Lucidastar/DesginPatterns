e5783658cd82a1a5e8af14f1fbcf797675b1e580
Lucidastar <lucidahxx@gmail.com>
2018-07-24 16:25:25 +0800

    更新了单例模式的解释

--- DesginSingleton/src/com/lucidastar/singleton/ThreadSafeDoubleCheckLocking.java
+++ DesginSingleton/src/com/lucidastar/singleton/ThreadSafeDoubleCheckLocking.java
@@ -33,10 +33,12 @@ public final class ThreadSafeDoubleCheckLocking {
 	        // Again assign the instance to local variable to check if it was initialized by some other thread
 	        // while current thread was blocked to enter the locked zone. If it was initialized then we can 
 	        // return the previously created instance just like the previous null check.
+	    	  //再次将实例赋值给局部变量，以检查它是否被其他线程初始化，而当前线程被阻塞以进入锁定区域。如果初始化了，那么我们可以像以前的空校验一样返回先前创建的实例。
 	        result = instance;
 	        if (result == null) {
 	          // The instance is still not initialized so we can safely (no other thread can enter this zone)
 	          // create an instance and make it our singleton instance.
+	        //这个实例仍然没有初始化，所以我们可以安全地（没有其他线程可以进入这个区域）创建一个实例，并使它成为我们的单实例。
 	          instance = result = new ThreadSafeDoubleCheckLocking();
 	        }
 	      }
